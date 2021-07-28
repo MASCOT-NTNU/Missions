55f34ee9490bb90713f0a4c050dcb64fe39a174b
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-07-28 10:08:23 +0200

    added new reporting

--- Adaptive_script/Data_extraction.py
+++ Adaptive_script/Data_extraction.py
@@ -1,13 +1,12 @@
-# datapath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/May27/Data/'
+datapath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/May27/Data/'
 import matplotlib.pyplot as plt
-
-datapath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/July06/Adaptive/Data/'
-figpath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/July06/Adaptive/fig/'
+# datapath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/July06/Adaptive/Data/'
+# figpath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/July06/Adaptive/fig/'
 import numpy as np
 import pandas as pd
 from datetime import datetime
 import os
-from usr_func import *
+from Adaptive_script.usr_func import *
 
 
 #% Data extraction from the raw data
@@ -83,78 +82,7 @@ time_mission = np.array(time_mission).reshape(-1, 1)
 
 datasheet = np.hstack((time_mission, lat, lon, x, y, z, d, sal, temp))
 
-#%%
-import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize
-from matplotlib.cm import ScalarMappable
-from gmplot import GoogleMapPlotter
-from random import random
 
-class CustomGoogleMapPlotter(GoogleMapPlotter):
-    def __init__(self, center_lat, center_lng, zoom, apikey='AIzaSyAZ_VZXoJULTFQ9KSPg1ClzHEFjyPbJUro',
-                 map_type='satellite'):
-        if apikey == '':
-            try:
-                with open('apikey.txt', 'r') as apifile:
-                    apikey = apifile.readline()
-            except FileNotFoundError:
-                pass
-        print(apikey)
-        GoogleMapPlotter(center_lat, center_lng, zoom, apikey=apikey)
-        super().__init__(center_lat, center_lng, zoom, apikey = apikey)
-
-        self.map_type = map_type
-        assert(self.map_type in ['roadmap', 'satellite', 'hybrid', 'terrain'])
-
-    def write_map(self,  f):
-        f.write('\t\tvar centerlatlng = new google.maps.LatLng(%f, %f);\n' %
-                (self.center[0], self.center[1]))
-        f.write('\t\tvar myOptions = {\n')
-        f.write('\t\t\tzoom: %d,\n' % (self.zoom))
-        f.write('\t\t\tcenter: centerlatlng,\n')
-
-        # Change this line to allow different map types
-        f.write('\t\t\tmapTypeId: \'{}\'\n'.format(self.map_type))
-
-        f.write('\t\t};\n')
-        f.write(
-            '\t\tvar map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);\n')
-        f.write('\n')
-
-    def color_scatter(self, lats, lngs, values=None, colormap='coolwarm',
-                      size=None, marker=False, s=None, **kwargs):
-        def rgb2hex(rgb):
-            """ Convert RGBA or RGB to #RRGGBB """
-            rgb = list(rgb[0:3]) # remove alpha if present
-            rgb = [int(c * 255) for c in rgb]
-            hexcolor = '#%02x%02x%02x' % tuple(rgb)
-            return hexcolor
-
-        if values is None:
-            colors = [None for _ in lats]
-        else:
-            cmap = plt.get_cmap(colormap)
-            norm = Normalize(vmin=min(values), vmax=max(values))
-            scalar_map = ScalarMappable(norm=norm, cmap=cmap)
-            colors = [rgb2hex(scalar_map.to_rgba(value)) for value in values]
-        for lat, lon, c in zip(lats, lngs, colors):
-            self.scatter(lats=[lat], lngs=[lon], c=c, size=size, marker=marker,
-                         s=s, **kwargs)
-
-from usr_func import *
-
-initial_zoom = 12
-# apikey = 'AIzaSyDkWNSq_EKnrV9qP6thJe5Y8a5kVLKEjUI'
-apikey = 'AIzaSyAZ_VZXoJULTFQ9KSPg1ClzHEFjyPbJUro'
-gmap = CustomGoogleMapPlotter(lat[0, 0], lon[0, 0], initial_zoom, map_type='satellite', apikey = apikey)
-gmap.color_scatter(lat[:, 0].tolist(), lon[:, 0].tolist(), sal.squeeze(), size = 10, colormap='hsv')
-
-gmap.draw(figpath + "sal.html")
-plt.figure()
-plt.scatter(lon, lat, c = sal, cmap = 'hsv')
-plt.colorbar()
-plt.savefig(figpath + "sal.pdf")
-plt.show()
 #%%
 # np.savetxt(figpath + "../data.txt", datasheet, delimiter = ",")
 
