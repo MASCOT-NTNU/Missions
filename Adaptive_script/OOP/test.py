7c60518242ea7a43fb023429d1f6bb099a64c337
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-08-06 13:08:27 +0200

    finished prior

--- Adaptive_script/OOP/test.py
+++ Adaptive_script/OOP/test.py
@@ -1,7 +1,7 @@
 
 
 from GP import GaussianProcess
-
+from sklearn.linear_model import LinearRegression
 # a = Grid()
 # a.print_var()
 # a.checkGrid()
@@ -11,6 +11,7 @@ from GP import GaussianProcess
 # b = GaussianProcess()
 import numpy as np
 class Prior(GaussianProcess):
+    AUVdata = None
     mu_sal_prior = None
     mu_temp_prior = None
     Sigma_sal_prior = None
@@ -18,21 +19,40 @@ class Prior(GaussianProcess):
     lat_grid = np.zeros([GaussianProcess.N1, GaussianProcess.N2])
     lon_grid = np.zeros([GaussianProcess.N1, GaussianProcess.N2])
     coord_grid = np.zeros([GaussianProcess.N1 * GaussianProcess.N2, 2])
+    depth_tolerance = 0.25 # tolerance +/- in depth, 0.5 m == [0.25 ~ 0.75]m
+
+    SINMOD_datapath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Adaptive_script/samples_2020.05.01.nc"
+    SINMOD_Data = None
+
+    beta0 = None
+    beta1 = None
+    mu_prior_sal = None
+    mu_prior_temp = None
 
     def __init__(self):
         GaussianProcess.__init__(self)
         self.generateCoordinates()
-        self.print_var()
+        self.getAUVData()
+        self.getSINMODData()
+        self.getCoefficients()
+        self.saveCoef()
 
-    def print_var(self):
-        print("lat_grid: ", Prior.lat_grid.shape)
-        print("lon_grid: ", Prior.lon_grid.shape)
-        print("coord_grid: ", Prior.coord_grid.shape)
-        # print("mu_sal_prior: ", Prior.mu_sal_prior.shape)
-        # print("mu_temp_prior: ", Prior.mu_temp_prior.shape)
-        # print(GaussianProcess.N2)
-        # print(GaussianProcess.grid.shape)
-        print(Prior.coord_grid)
+        # print(Prior.beta0)
+        # print(Prior.beta1)
+        print(Prior.mu_prior_sal.shape)
+        # print(Prior.AUVdata.shape)
+        # self.print_var()
+
+    # def print_var(self):
+    #     print("lat_grid: ", Prior.lat_grid.shape)
+    #     print("lon_grid: ", Prior.lon_grid.shape)
+    #     print("coord_grid: ", Prior.coord_grid.shape)
+    #     # print("mu_sal_prior: ", Prior.mu_sal_prior.shape)
+    #     # print("mu_temp_prior: ", Prior.mu_temp_prior.shape)
+    #     # print(GaussianProcess.N2)
+    #     # print(GaussianProcess.grid.shape)
+    #     # print(Prior.coord_grid)
+    #     print("data is ", Prior.AUVdata)
 
     def generateCoordinates(self):
         for i in range(GaussianProcess.N1):
@@ -48,35 +68,84 @@ class Prior(GaussianProcess):
                                                      np.cos(GaussianProcess.deg2rad(self, Prior.lat_grid[i, j]))))
         Prior.coord_grid = np.hstack((Prior.lat_grid.reshape(-1, 1), Prior.lon_grid.reshape(-1, 1)))
 
-    #
-    # def getCoefficients(self):
-    #     timestamp = data[:, 0].reshape(-1, 1)
-    #     # lat_auv = rad2deg(data[:, 1].reshape(-1, 1))
-    #     # lon_auv = rad2deg(data[:, 2].reshape(-1, 1))
-    #     lat_auv = data[:, 1].reshape(-1, 1)
-    #     lon_auv = data[:, 2].reshape(-1, 1)
-    #     # print(lat_auv, lon_auv)
-    #
-    #     xauv = data[:, 3].reshape(-1, 1)
-    #     yauv = data[:, 4].reshape(-1, 1)
-    #     zauv = data[:, 5].reshape(-1, 1)
-    #     depth_auv = data[:, 6].reshape(-1, 1)
-    #     sal_auv = data[:, 7].reshape(-1, 1)
-    #     temp_auv = data[:, 8].reshape(-1, 1)
-    #     lat_auv = lat_auv + rad2deg(xauv * np.pi * 2.0 / circumference)
-    #     lon_auv = lon_auv + rad2deg(yauv * np.pi * 2.0 / (circumference * np.cos(deg2rad(lat_auv))))
-    #
-    #     depthl = np.array(depth) - err_bound
-    #     depthu = np.array(depth) + err_bound
-    #
-    #     # print(depthl, depthu)
-    #
-    #     beta0 = np.zeros([len(depth), 2])
-    #     beta1 = np.zeros([len(depth), 2])
-    #     sal_residual = []
-    #     temp_residual = []
-    #     x_loc = []
-    #     y_loc = []
+    def getAUVData(self):
+        Prior.AUVdata = np.loadtxt('data.txt', delimiter = ",")
+
+    def getSINMODData(self):
+        import netCDF4
+        Prior.SINMOD_Data = netCDF4.Dataset(Prior.SINMOD_datapath)
+
+    def getCoefficients(self):
+        # timestamp = Prior.AUVdata[:, 0].reshape(-1, 1)
+        lat_auv_origin = self.rad2deg(Prior.AUVdata[:, 1]).reshape(-1, 1)
+        lon_auv_origin = self.rad2deg(Prior.AUVdata[:, 2]).reshape(-1, 1)
+        xauv = Prior.AUVdata[:, 3].reshape(-1, 1)
+        yauv = Prior.AUVdata[:, 4].reshape(-1, 1)
+        # zauv = Prior.AUVdata[:, 5].reshape(-1, 1)
+        depth_auv = Prior.AUVdata[:, 6].reshape(-1, 1)
+        sal_auv = Prior.AUVdata[:, 7].reshape(-1, 1)
+        temp_auv = Prior.AUVdata[:, 8].reshape(-1, 1)
+        lat_auv = lat_auv_origin + self.rad2deg(xauv * np.pi * 2.0 / self.circumference)
+        lon_auv = lon_auv_origin + self.rad2deg(yauv * np.pi * 2.0 / (self.circumference * np.cos(self.deg2rad(lat_auv))))
+
+        depthl = np.array(self.depth_obs) - self.depth_tolerance
+        depthu = np.array(self.depth_obs) + self.depth_tolerance
+
+        Prior.beta0 = np.zeros([len(self.depth_obs), 2])
+        Prior.beta1 = np.zeros([len(self.depth_obs), 2])
+        sal_residual = []
+        temp_residual = []
+
+        Prior.mu_prior_sal = []
+        Prior.mu_prior_temp = []
+        for i in range(len(self.depth_obs)):
+            # sort out AUV data
+            ind_obs = (depthl[i] <= depth_auv) & (depth_auv <= depthu[i])
+            lat_obs = lat_auv[ind_obs].reshape(-1, 1)
+            lon_obs = lon_auv[ind_obs].reshape(-1, 1)
+            sal_obs = sal_auv[ind_obs].reshape(-1, 1)
+            temp_obs = temp_auv[ind_obs].reshape(-1, 1)
+
+            # sort out SINMOD data
+            salinity = np.mean(Prior.SINMOD_Data['salinity'][:, :, :, :], axis=0)  # time averaging of salinity
+            temperature = np.mean(Prior.SINMOD_Data['temperature'][:, :, :, :], axis=0) - 273.15  # time averaging of temperature
+            depth_sinmod = np.array(Prior.SINMOD_Data['zc'])  # depth from SINMOD
+            lat_sinmod = np.array(Prior.SINMOD_Data['gridLats'][:, :]).reshape(-1, 1)  # lat from SINMOD
+            lon_sinmod = np.array(Prior.SINMOD_Data['gridLons'][:, :]).reshape(-1, 1)  # lon from SINMOD
+            sal_sinmod = np.zeros([sal_obs.shape[0], 1])
+            temp_sinmod = np.zeros([temp_obs.shape[0], 1])
+
+            for j in range(sal_obs.shape[0]):
+                ind_depth = np.where(np.array(depth_sinmod) == self.depth_obs[i])[0][0]
+                idx = np.argmin((lat_sinmod - lat_obs[j]) ** 2 + (lon_sinmod - lon_obs[j]) ** 2)
+                sal_sinmod[j] = salinity[ind_depth].reshape(-1, 1)[idx]
+                temp_sinmod[j] = temperature[ind_depth].reshape(-1, 1)[idx]
+
+            # compute the coef for salinity
+            sal_modelX = np.hstack((np.ones_like(sal_sinmod), sal_sinmod))
+            sal_modelY = sal_obs
+            Beta_sal = np.linalg.solve((sal_modelX.T @ sal_modelX), (sal_modelX.T @ sal_modelY))
+            # compute the coef for temperature
+            temp_modelX = np.hstack((np.ones_like(temp_sinmod), temp_sinmod))
+            temp_modelY = temp_obs
+            Beta_temp = np.linalg.solve((temp_modelX.T @ temp_modelX), (temp_modelX.T @ temp_modelY))
+
+            Prior.beta0[i, :] = np.hstack((Beta_sal[0], Beta_temp[0]))
+            Prior.beta1[i, :] = np.hstack((Beta_sal[1], Beta_temp[1]))
+            Prior.mu_prior_sal.append(Prior.beta0[i, 0] + Prior.beta1[i, 0] * sal_sinmod)
+            Prior.mu_prior_temp.append(Prior.beta0[i, 1] + Prior.beta1[i, 1] * temp_sinmod)
+
+            sal_residual.append(sal_obs - Beta_sal[0] - Beta_sal[1] * sal_sinmod)
+            temp_residual.append(temp_obs - Beta_temp[0] - Beta_temp[1] * temp_sinmod)
+
+        Prior.mu_prior_sal = np.array(Prior.mu_prior_sal).reshape(-1, 1)
+        Prior.mu_prior_temp = np.array(Prior.mu_prior_temp).reshape(-1, 1)
+
+    def saveCoef(self):
+        np.savetxt("beta0.txt", Prior.beta0, delimiter=",")
+        np.savetxt("beta1.txt", Prior.beta1, delimiter=",")
+        np.savetxt("mu_prior_sal.txt", Prior.mu_prior_sal, delimiter=",")
+        np.savetxt("mu_prior_temp.txt", Prior.mu_prior_temp, delimiter=",")
 
     def checkCoords(self):
         from gmplot import GoogleMapPlotter
@@ -91,6 +160,31 @@ class Prior(GaussianProcess):
 c = Prior()
 # c.checkCoords()
 
+#%%
+import numpy as np
+from sklearn.linear_model import LinearRegression
+print("hello")
+x = np.arange(10).reshape(-1, 1)
+y = (np.arange(10) + 2 * np.random.rand(10)).reshape(-1, 1)
+
+import matplotlib.pyplot as plt
+ X = np.hstack((np.ones_like(x), x))
+
+beta = np.linalg.solve((X.T @ X), X.T @ y)
+Y = beta[0] + beta[1] * x
+model = LinearRegression()
+model.fit(x, y)
+print(model.intercept_, model.coef_)
+plt.plot(x, y, 'k.')
+plt.plot(x, Y, 'r-')
+
+plt.show()
+print(beta)
+
+#%%
+a = np.loadtxt("data.txt", delimiter = ",")
+
+#%%
 # class PathPlanner(Grid, GP):
 #     def __init__(self, Grid, GP):
 #         # super().__init__(Grid)
