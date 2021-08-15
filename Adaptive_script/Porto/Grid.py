4b21ff9a133d776efb97f4d9e3d2c7c92e41637e
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-08-15 15:18:30 +0200

    trying to krige the field

--- Adaptive_script/Porto/Grid.py
+++ Adaptive_script/Porto/Grid.py
@@ -36,8 +36,8 @@ class Grid:
     XLIM = [0, distance]  # limit for the x axis
     YLIM = [0, distance]  # limit for the y axis
     ZLIM = [depth_obs[0], depth_obs[-1]]  # limit for the z axis
-    x = np.linspace(XLIM[0], XLIM[-1], N1)  # x coordinates
-    y = np.linspace(YLIM[0], YLIM[-1], N1)  # y coordinates
+    x = np.linspace(XLIM[0], XLIM[-1], N1) # x coordinates
+    y = np.linspace(YLIM[0], YLIM[-1], N1) # y coordinates
     z = np.array(depth_obs).reshape(-1, 1)  # z coordinates
     dx = x[1] - x[0]
     dy = y[1] - y[0]
@@ -45,11 +45,14 @@ class Grid:
     R = None # rotational matrix
     box = None # bounding box
     grid = None # grid coordinates xy
+    grid_coord = None
 
     def __init__(self):
-        Grid.generateBox(self)
-        Grid.generateGrid(self)
-        Grid.print_grid(self)
+        self.computeR()
+        self.generateBox()
+        self.generateGrid()
+        self.generateGridCoord()
+        self.print_grid()
         print("Grid is generated successfully!\n\n")
 
     def print_grid(self):
@@ -67,29 +70,29 @@ class Grid:
         print("ZLIM: ", self.ZLIM)
 
     def set_distance(self, value):
-        Grid.distance = value
+        self.distance = value
 
     def set_depth_tolerance(self, value):
-        Grid.depth_tolerance = value
+        self.depth_tolerance = value
 
     def set_origin(self, lat, lon):
-        Grid.lat_origin = lat
-        Grid.lon_origin = lon
+        self.lat_origin = lat
+        self.lon_origin = lon
 
     def set_circumference(self, value):
-        Grid.circumference = value
+        self.circumference = value
 
     def set_depthObs(self, value):
-        Grid.depth_obs = value
+        self.depth_obs = value
 
     def set_alpha(self, value):
-        Grid.alpha = value
+        self.alpha = value
 
     def set_N1(self, value):
-        Grid.N1 = value
+        self.N1 = value
 
     def set_N2(self, value):
-        Grid.N2 = value
+        self.N2 = value
 
     def deg2rad(self, deg):
         return deg / 180 * np.pi
@@ -97,41 +100,59 @@ class Grid:
     def rad2deg(self, rad):
         return rad / np.pi * 180
 
+    def computeR(self):
+        # rotational matrix convert tilted x'y' to regual xy
+        self.R = np.array([[np.cos(self.deg2rad(self.alpha)), np.sin(self.deg2rad(self.alpha))],
+                          [-np.sin(self.deg2rad(self.alpha)), np.cos(self.deg2rad(self.alpha))]])
+        print("Rotational matrix is computed:")
+        print(self.R)
+
     def BBox(self):
         '''
         :return: Box of the operational regions
         '''
-        lat4 = self.deg2rad(Grid.lat_origin)  # the right bottom corner
-        lon4 = self.deg2rad(Grid.lon_origin)  # the right bottom corner
+        lat4 = self.deg2rad(self.lat_origin)  # the right bottom corner
+        lon4 = self.deg2rad(self.lon_origin)  # the right bottom corner
 
-        lat2 = lat4 + Grid.distance * np.sin(self.deg2rad(Grid.alpha)) / Grid.circumference * 2 * np.pi
-        lat1 = lat2 + Grid.distance * np.cos(self.deg2rad(Grid.alpha)) / Grid.circumference * 2 * np.pi
-        lat3 = lat4 + Grid.distance * np.sin(np.pi / 2 - self.deg2rad(Grid.alpha)) / Grid.circumference * 2 * np.pi
+        lat2 = lat4 + self.distance * np.sin(self.deg2rad(self.alpha)) / self.circumference * 2 * np.pi
+        lat1 = lat2 + self.distance * np.cos(self.deg2rad(self.alpha)) / self.circumference * 2 * np.pi
+        lat3 = lat4 + self.distance * np.sin(np.pi / 2 - self.deg2rad(self.alpha)) / self.circumference * 2 * np.pi
 
-        lon2 = lon4 + Grid.distance * np.cos(self.deg2rad(Grid.alpha)) / (Grid.circumference * np.cos(lat2)) * 2 * np.pi
-        lon3 = lon4 - Grid.distance * np.cos(np.pi / 2 - self.deg2rad(Grid.alpha)) / (
-                    Grid.circumference * np.cos(lat3)) * 2 * np.pi
-        lon1 = lon3 + Grid.distance * np.cos(self.deg2rad(Grid.alpha)) / (Grid.circumference * np.cos(lat1)) * 2 * np.pi
+        lon2 = lon4 + self.distance * np.cos(self.deg2rad(self.alpha)) / (self.circumference * np.cos(lat2)) * 2 * np.pi
+        lon3 = lon4 - self.distance * np.cos(np.pi / 2 - self.deg2rad(self.alpha)) / (
+                    self.circumference * np.cos(lat3)) * 2 * np.pi
+        lon1 = lon3 + self.distance * np.cos(self.deg2rad(self.alpha)) / (self.circumference * np.cos(lat1)) * 2 * np.pi
 
         box = np.vstack((np.array([lat1, lat2, lat3, lat4]), np.array([lon1, lon2, lon3, lon4]))).T
         return self.rad2deg(box)
 
     def GGrid(self):
-        Grid.R = np.array([[np.cos(self.deg2rad(Grid.alpha)), np.sin(self.deg2rad(Grid.alpha))],
-                      [-np.sin(self.deg2rad(Grid.alpha)), np.cos(self.deg2rad(Grid.alpha))]])
         grid = []
-        for k in range(Grid.N3):
-            for i in range(Grid.N1):
-                for j in range(Grid.N2):
-                    tempx, tempy = Grid.R @ np.vstack((Grid.x[i], Grid.y[j]))
-                    grid.append([tempx[0], tempy[0], Grid.depth_obs[k]])
+        for k in range(self.N3):
+            for i in range(self.N1):
+                for j in range(self.N2):
+                    tempx, tempy = self.R @ np.vstack((self.x[i], self.y[j]))
+                    grid.append([tempx[0], tempy[0], self.depth_obs[k]])
         return np.array(grid)
 
+    def generateGridCoord(self):
+        lat_grid = np.zeros([self.N1, self.N2])
+        lon_grid = np.zeros([self.N1, self.N2])
+        for i in range(self.N1):
+            for j in range(self.N2):
+                xnew, ynew = self.R @ np.vstack((self.x[i], self.y[j])) # rotate the xy into the new frame
+                lat_grid[i, j] = self.lat_origin + self.rad2deg(xnew * np.pi * 2.0 / self.circumference)
+                lon_grid[i, j] = self.lon_origin + self.rad2deg(ynew * np.pi * 2.0 /
+                                (self.circumference * np.cos(self.deg2rad(lat_grid[i, j]))))
+        self.grid_coord = np.hstack((lat_grid.reshape(-1, 1), lon_grid.reshape(-1, 1)))
+        print("Grid coordinates are generated, shape is ")
+        print(self.grid_coord.shape)
+
     def generateBox(self):
-        Grid.box = self.BBox()
+        self.box = self.BBox()
 
     def generateGrid(self):
-        Grid.grid = self.GGrid()
+        self.grid = self.GGrid()
 
     def checkBox(self):
         from gmplot import GoogleMapPlotter
@@ -152,7 +173,7 @@ class Grid:
         fig = make_subplots(rows=1, cols=1, specs=[[{'type': 'scene'}]])
         fig.add_trace(
             go.Scatter3d(
-                x=Grid.grid[:, 0], y=Grid.grid[:, 1], z=Grid.grid[:, -1],
+                x=self.grid[:, 0], y=self.grid[:, 1], z=self.grid[:, -1],
                 # marker=dict(
                 #     color="black",
                 #     showscale=False
@@ -170,7 +191,14 @@ class Grid:
         )
         plotly.offline.plot(fig, filename="grid.html", auto_open=True)
 
+    def checkGridCoord(self):
+        from gmplot import GoogleMapPlotter
+        initial_zoom = 12
+        apikey = 'AIzaSyAZ_VZXoJULTFQ9KSPg1ClzHEFjyPbJUro'
+        gmap = GoogleMapPlotter(self.lat_origin, self.lon_origin, initial_zoom, map_type='satellite', apikey=apikey)
+        gmap.scatter(self.grid_coord[:, 0], self.grid_coord[:, 1], color='#99ff00', size=20, marker=False)
+        gmap.draw("/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/MapPlot/map.html")
 
-
-
+a = Grid()
+a.checkGridCoord()
 
--- Adaptive_script/Porto/Prior.py
+++ Adaptive_script/Porto/Prior.py
@@ -1,3 +1,13 @@
+#! /usr/bin/env python3
+__author__ = "Yaolin Ge"
+__copyright__ = "Copyright 2021, The MASCOT Project, NTNU (https://wiki.math.ntnu.no/mascot)"
+__credits__ = ["Yaolin Ge"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Yaolin Ge"
+__email__ = "yaolin.ge@ntnu.no"
+__status__ = "UnderDevelopment"
+
 import numpy as np
 import os
 from Adaptive_script.Porto.GP import GaussianProcess
@@ -10,20 +20,15 @@ class Prior(GaussianProcess):
     Sigma_prior_temp = None
     beta0 = None
     beta1 = None
-    lat_grid = np.zeros([GaussianProcess.N1, GaussianProcess.N2])
-    lon_grid = np.zeros([GaussianProcess.N1, GaussianProcess.N2])
-    coord_grid = np.zeros([GaussianProcess.N1 * GaussianProcess.N2, 2])
-
+    SINMOD_Data = None
     SINMOD_datapath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Adaptive_script/samples_2020.05.01.nc"
     if os.path.exists(SINMOD_datapath):
         pass
     else:
         SINMOD_datapath = "/home/yaoling/MASCOT/adaframe/catkin_ws/src/adaframe_examples/scripts/samples_2020.05.01.nc"
-    SINMOD_Data = None
 
     def __init__(self):
         GaussianProcess.__init__(self)
-        self.generateCoordinates()
         self.getAUVData()
         self.getSINMODData()
         self.getCoefficients()
@@ -32,42 +37,27 @@ class Prior(GaussianProcess):
         self.print_Prior()
 
     def print_Prior(self):
-        print("lat_grid: ", self.lat_grid.shape)
-        print("lon_grid: ", self.lon_grid.shape)
-        print("coord_grid: ", self.coord_grid.shape)
+        print("coord_grid: ", self.grid_coord.shape)
         print("beta0: \n", self.beta0)
         print("beta1: \n", self.beta1)
         print("mu_sal_prior: ", self.mu_prior_sal.shape)
         print("mu_temp_prior: ", self.mu_prior_temp.shape)
         print("Prior is setup successfully!\n\n")
 
-    def generateCoordinates(self):
-        for i in range(GaussianProcess.N1):
-            for j in range(GaussianProcess.N2):
-                xnew, ynew = GaussianProcess.R @ np.vstack((GaussianProcess.x[i], GaussianProcess.y[j]))
-
-                Prior.lat_grid[i, j] = GaussianProcess.lat_origin + \
-                            GaussianProcess.rad2deg(self, xnew * np.pi * 2.0 /
-                                                    GaussianProcess.circumference)
-                Prior.lon_grid[i, j] = GaussianProcess.lon_origin + \
-                            GaussianProcess.rad2deg(self, ynew * np.pi * 2.0 /
-                                                    (GaussianProcess.circumference *
-                                                     np.cos(GaussianProcess.deg2rad(self, Prior.lat_grid[i, j]))))
-        Prior.coord_grid = np.hstack((Prior.lat_grid.reshape(-1, 1), Prior.lon_grid.reshape(-1, 1)))
-
     def getAUVData(self):
-        Prior.AUVdata = np.loadtxt('Adaptive_script/Porto/data.txt', delimiter = ",")
+        self.AUVdata = np.loadtxt('Adaptive_script/Porto/data.txt', delimiter = ",")
 
     def getSINMODData(self):
         import netCDF4
-        Prior.SINMOD_Data = netCDF4.Dataset(Prior.SINMOD_datapath)
+        print(self.SINMOD_datapath)
+        self.SINMOD_Data = netCDF4.Dataset(self.SINMOD_datapath)
 
     def getSINMODFromCoordsDepth(self, coordinates, depth):
-        salinity = np.mean(Prior.SINMOD_Data['salinity'][:, :, :, :], axis=0)
-        temperature = np.mean(Prior.SINMOD_Data['temperature'][:, :, :, :], axis=0) - 273.15
-        depth_sinmod = np.array(Prior.SINMOD_Data['zc'])
-        lat_sinmod = np.array(Prior.SINMOD_Data['gridLats'][:, :]).reshape(-1, 1)
-        lon_sinmod = np.array(Prior.SINMOD_Data['gridLons'][:, :]).reshape(-1, 1)
+        salinity = np.mean(self.SINMOD_Data['salinity'][:, :, :, :], axis=0)
+        temperature = np.mean(self.SINMOD_Data['temperature'][:, :, :, :], axis=0) - 273.15
+        depth_sinmod = np.array(self.SINMOD_Data['zc'])
+        lat_sinmod = np.array(self.SINMOD_Data['gridLats'][:, :]).reshape(-1, 1)
+        lon_sinmod = np.array(self.SINMOD_Data['gridLons'][:, :]).reshape(-1, 1)
         sal_sinmod = np.zeros([coordinates.shape[0], 1])
         temp_sinmod = np.zeros([coordinates.shape[0], 1])
 
@@ -80,23 +70,23 @@ class Prior(GaussianProcess):
         return sal_sinmod, temp_sinmod
 
     def getCoefficients(self):
-        # timestamp = Prior.AUVdata[:, 0].reshape(-1, 1)
-        lat_auv_origin = self.rad2deg(Prior.AUVdata[:, 1]).reshape(-1, 1)
-        lon_auv_origin = self.rad2deg(Prior.AUVdata[:, 2]).reshape(-1, 1)
-        xauv = Prior.AUVdata[:, 3].reshape(-1, 1)
-        yauv = Prior.AUVdata[:, 4].reshape(-1, 1)
-        # zauv = Prior.AUVdata[:, 5].reshape(-1, 1)
-        depth_auv = Prior.AUVdata[:, 6].reshape(-1, 1)
-        sal_auv = Prior.AUVdata[:, 7].reshape(-1, 1)
-        temp_auv = Prior.AUVdata[:, 8].reshape(-1, 1)
+        # timestamp = self.AUVdata[:, 0].reshape(-1, 1)
+        lat_auv_origin = self.rad2deg(self.AUVdata[:, 1]).reshape(-1, 1)
+        lon_auv_origin = self.rad2deg(self.AUVdata[:, 2]).reshape(-1, 1)
+        xauv = self.AUVdata[:, 3].reshape(-1, 1)
+        yauv = self.AUVdata[:, 4].reshape(-1, 1)
+        # zauv = self.AUVdata[:, 5].reshape(-1, 1)
+        depth_auv = self.AUVdata[:, 6].reshape(-1, 1)
+        sal_auv = self.AUVdata[:, 7].reshape(-1, 1)
+        temp_auv = self.AUVdata[:, 8].reshape(-1, 1)
         lat_auv = lat_auv_origin + self.rad2deg(xauv * np.pi * 2.0 / self.circumference)
         lon_auv = lon_auv_origin + self.rad2deg(yauv * np.pi * 2.0 / (self.circumference * np.cos(self.deg2rad(lat_auv))))
 
         depthl = np.array(self.depth_obs) - self.depth_tolerance
         depthu = np.array(self.depth_obs) + self.depth_tolerance
 
-        Prior.beta0 = np.zeros([len(self.depth_obs), 2])
-        Prior.beta1 = np.zeros([len(self.depth_obs), 2])
+        self.beta0 = np.zeros([len(self.depth_obs), 2])
+        self.beta1 = np.zeros([len(self.depth_obs), 2])
         sal_residual = []
         temp_residual = []
 
@@ -121,36 +111,28 @@ class Prior(GaussianProcess):
             temp_modelY = temp_obs
             Beta_temp = np.linalg.solve((temp_modelX.T @ temp_modelX), (temp_modelX.T @ temp_modelY))
 
-            Prior.beta0[i, :] = np.hstack((Beta_sal[0], Beta_temp[0]))
-            Prior.beta1[i, :] = np.hstack((Beta_sal[1], Beta_temp[1]))
+            self.beta0[i, :] = np.hstack((Beta_sal[0], Beta_temp[0]))
+            self.beta1[i, :] = np.hstack((Beta_sal[1], Beta_temp[1]))
 
             sal_residual.append(sal_obs - Beta_sal[0] - Beta_sal[1] * sal_sinmod)
             temp_residual.append(temp_obs - Beta_temp[0] - Beta_temp[1] * temp_sinmod)
 
     def get_mu_prior(self):
-        Prior.mu_prior_sal = []
-        Prior.mu_prior_temp = []
+        self.mu_prior_sal = []
+        self.mu_prior_temp = []
         for i in range(len(self.depth_obs)):
-            sal_sinmod, temp_sinmod = self.getSINMODFromCoordsDepth(Prior.coord_grid, self.depth_obs[i])
-            Prior.mu_prior_sal.append(Prior.beta0[i, 0] + Prior.beta1[i, 0] * sal_sinmod)
-            Prior.mu_prior_temp.append(Prior.beta0[i, 1] + Prior.beta1[i, 1] * temp_sinmod)
-        Prior.mu_prior_sal = np.array(Prior.mu_prior_sal).reshape(-1, 1)
-        Prior.mu_prior_temp = np.array(Prior.mu_prior_temp).reshape(-1, 1)
+            sal_sinmod, temp_sinmod = self.getSINMODFromCoordsDepth(self.grid_coord, self.depth_obs[i])
+            self.mu_prior_sal.append(self.beta0[i, 0] + self.beta1[i, 0] * sal_sinmod)
+            self.mu_prior_temp.append(self.beta0[i, 1] + self.beta1[i, 1] * temp_sinmod)
+        self.mu_prior_sal = np.array(self.mu_prior_sal).reshape(-1, 1)
+        self.mu_prior_temp = np.array(self.mu_prior_temp).reshape(-1, 1)
 
     def saveCoef(self):
-        np.savetxt("beta0.txt", Prior.beta0, delimiter=",")
-        np.savetxt("beta1.txt", Prior.beta1, delimiter=",")
-        np.savetxt("mu_prior_sal.txt", Prior.mu_prior_sal, delimiter=",")
-        np.savetxt("mu_prior_temp.txt", Prior.mu_prior_temp, delimiter=",")
+        np.savetxt("beta0.txt", self.beta0, delimiter=",")
+        np.savetxt("beta1.txt", self.beta1, delimiter=",")
+        np.savetxt("mu_prior_sal.txt", self.mu_prior_sal, delimiter=",")
+        np.savetxt("mu_prior_temp.txt", self.mu_prior_temp, delimiter=",")
 
-    def checkCoords(self):
-        from gmplot import GoogleMapPlotter
-        initial_zoom = 12
-        apikey = 'AIzaSyAZ_VZXoJULTFQ9KSPg1ClzHEFjyPbJUro'
-        gmap = GoogleMapPlotter(self.lat_origin, self.lon_origin, initial_zoom, map_type='satellite', apikey=apikey)
-        gmap.scatter(self.coord_grid[:, 0], self.coord_grid[:, 1], color='#99ff00', size=20, marker=False)
-        gmap.draw("/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/MapPlot/map.html")
-
-if __name__ == "__main__":
-    prior = Prior()
-    print("Ferdig med prior")
+# if __name__ == "__main__":
+#     prior = Prior()
+#     print("Ferdig med prior")
