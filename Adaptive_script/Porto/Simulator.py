faaed55caac8dbdc60569b30993d7b643e088a24
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-09-13 17:57:40 +0200

    Finished_3D

--- Adaptive_script/Porto/Simulator.py
+++ Adaptive_script/Porto/Simulator.py
@@ -31,7 +31,6 @@ class Simulator(GP_Poly):
         print("range of neighbours: ", self.distance_neighbours)
         self.travelled_waypoints = 0
         self.load_prior()
-        self.plot_prior()
         if self.SIMULATION:
             self.move_to_starting_loc()
             self.run()
@@ -48,12 +47,14 @@ class Simulator(GP_Poly):
             print(self.figpath + " is already existed")
 
     def load_prior(self):
-        self.lat_loc = self.lat_selected[:, 0] # select the top layer for simulation
-        self.lon_loc = self.lon_selected[:, 0]
-        # self.lat_loc = self.lat_selected[:, 0]
-        self.salinity_loc = self.salinity_selected[:, 0]
-        self.mu_prior = self.salinity_loc.reshape(-1, 1)
+        self.lat_loc = self.lat_selected.reshape(-1, 1) # select the top layer for simulation
+        self.lon_loc = self.lon_selected.reshape(-1, 1)
+        self.depth_loc = self.depth_selected.reshape(-1, 1)
+        self.salinity_loc = self.salinity_selected.reshape(-1, 1)
+        self.mu_prior = self.salinity_loc
         self.Sigma_prior = self.Sigma_sal
+        print("Sigma_prior: ", self.Sigma_prior.shape)
+        print("Sigma sal: ", self.Sigma_sal.shape)
         self.mu_real = self.mu_prior + np.linalg.cholesky(self.Sigma_prior) @ np.random.randn(len(self.mu_prior)).reshape(-1, 1)
         self.mu_cond = self.mu_prior
         self.Sigma_cond = self.Sigma_prior
@@ -64,25 +65,6 @@ class Simulator(GP_Poly):
         print("mu_cond shape is: ", self.mu_cond.shape)
         print("Sigma_cond shape is: ", self.Sigma_cond.shape)
         print("N: ", self.N)
-
-    def plot_prior(self):
-        plt.figure(figsize = (40, 10))
-        plt.subplot(121)
-        plt.scatter(self.lon_loc, self.lat_loc, c = self.mu_prior, s = 500, cmap = "Paired")
-        plt.colorbar()
-        plt.title("Prior mean")
-        plt.xlabel("Lon [deg]")
-        plt.ylabel("Lat [deg]")
-        # plt.subplot(132)
-        # plt.imshow(self.Sigma_prior)
-        # plt.colorbar()
-        plt.subplot(122)
-        plt.scatter(self.lon_loc, self.lat_loc, c=self.mu_real, s = 500, cmap="Paired")
-        plt.colorbar()
-        plt.title("true mean")
-        plt.xlabel("Lon [deg]")
-        plt.ylabel("Lat [deg]")
-        plt.show()
 
     def updateF(self, ind):
         self.F = np.zeros([1, self.N])
@@ -97,21 +79,22 @@ class Simulator(GP_Poly):
     def move_to_starting_loc(self):
         EP_Prior = self.EP_1D(self.mu_prior, self.Sigma_prior, self.Threshold_S)
         ep_criterion = 0.5 # excursion probability close to 0.5
-        # self.ind_start = (np.abs(EP_Prior - ep_criterion)).argmin()
-        self.ind_start = 10
+        self.ind_start = (np.abs(EP_Prior - ep_criterion)).argmin()
+        if self.ind_start == 0:
+            self.ind_start = np.random.randint(self.N)
         self.ind_next = self.ind_start
         self.updateF(self.ind_next)
         self.data_path_lat.append(self.lat_loc[self.ind_start])
         self.data_path_lon.append(self.lon_loc[self.ind_start])
         self.updateWaypoint()
-        # self.updateWaypoint()
 
     def find_candidates_loc(self):
         '''
         find the candidates location based on distance coverage
         '''
         delta_x, delta_y = self.latlon2xy(self.lat_loc, self.lon_loc, self.lat_loc[self.ind_now], self.lon_loc[self.ind_now]) # using the distance
-        self.distance_vector = np.sqrt(delta_x ** 2 + delta_y ** 2)
+        delta_z = self.depth_loc - self.depth_loc[self.ind_now] # depth distance in z-direction
+        self.distance_vector = np.sqrt(delta_x ** 2 + delta_y ** 2 + delta_z ** 2)
         self.ind_cand = np.where(self.distance_vector <= self.distance_neighbours + self.distanceTolerance)[0]
 
     def GPupd(self, y_sampled):
@@ -136,20 +119,24 @@ class Simulator(GP_Poly):
         id = [] # ind vector for containing the filtered desired candidate location
         t1 = time.time()
         dx1, dy1 = self.latlon2xy(self.lat_loc[self.ind_now], self.lon_loc[self.ind_now], self.lat_loc[self.ind_pre], self.lon_loc[self.ind_pre])
+        dz1 = self.depth_loc[self.ind_now] - self.depth_loc[self.ind_pre]
         lat_cand_plot = []
         lon_cand_plot = []
-        vec1 = np.array([dx1, dy1])
+        depth_cand_plot = []
+        vec1 = np.array([dx1, dy1, dz1]).squeeze()
         print("vec1 :", vec1)
         for i in range(len(self.ind_cand)):
             if self.ind_cand[i] != self.ind_now:
                 dx2, dy2 = self.latlon2xy(self.lat_loc[self.ind_cand[i]], self.lon_loc[self.ind_cand[i]], self.lat_loc[self.ind_now], self.lon_loc[self.ind_now])
-                vec2 = np.array([dx2, dy2])
+                dz2 = self.depth_loc[self.ind_cand[i]] - self.depth_loc[self.ind_now]
+                vec2 = np.array([dx2, dy2, dz2]).squeeze()
                 print("vec2: ", vec2)
                 if np.dot(vec1, vec2) > 0:
                     print("Product: ", np.dot(vec1, vec2))
                     id.append(self.ind_cand[i])
                     lat_cand_plot.append(self.lat_loc[self.ind_cand[i]])
                     lon_cand_plot.append(self.lon_loc[self.ind_cand[i]])
+                    depth_cand_plot.append(self.depth_loc[self.ind_cand[i]])
                     print("The candidate location: ", self.ind_cand[i], self.ind_now)
                     print("Candloc: ", [self.lat_loc[self.ind_cand[i]], self.lon_loc[self.ind_cand[i]]])
                     print("NowLoc: ", [self.lat_loc[self.ind_now], self.lon_loc[self.ind_now]])
@@ -167,66 +154,15 @@ class Simulator(GP_Poly):
 
         if len(eibv) == 0: # in case it is in the corner and not found any valid candidate locations
             print("No valid candidates found: ")
-            self.ind_next = np.where(self.EP_1D(mu, Sig, self.Threshold_S) == .5)[0][0]
+            self.ind_next = np.abs(self.EP_1D(mu, Sig, self.Threshold_S) - .5).argmin() # if not found next, use the other one
         else:
             print("The EIBV for the candidate location: ", np.array(eibv))
             self.ind_next = self.ind_cand[np.argmin(np.array(eibv))]
         print("ind_next: ", self.ind_next)
 
-        if self.SIMULATION:
-            plt.figure(figsize=(40, 40))
-            plt.subplot(221)
-            plt.scatter(self.lon_loc, self.lat_loc, c=self.mu_cond, vmin=15, vmax=36, s = 500, alpha = 1, cmap="Paired")
-            plt.plot(self.lon_loc[self.ind_pre], self.lat_loc[self.ind_pre], 'kx', markersize=10)
-            plt.plot(self.lon_loc[self.ind_cand], self.lat_loc[self.ind_cand], 'go', markersize=10)
-            plt.plot(self.lon_loc[self.ind_next], self.lat_loc[self.ind_next], 'rs', markersize=10)
-            plt.plot(self.data_path_lon, self.data_path_lat, 'b-', linewidth = 5)
-            plt.colorbar()
-            plt.xlabel("Lon [deg]")
-            plt.ylabel("Lat [deg]")
-            plt.title("Mu cond")
-
-            plt.subplot(222)
-            err = np.sqrt(np.diag(self.Sigma_cond))
-            plt.scatter(self.lon_loc, self.lat_loc, c=err, s = 500, alpha = .5)
-            plt.plot(self.lon_loc[self.ind_pre], self.lat_loc[self.ind_pre], 'kx', markersize=10)
-            plt.plot(self.lon_loc[self.ind_cand], self.lat_loc[self.ind_cand], 'go', markersize=10)
-            plt.plot(self.lon_loc[self.ind_next], self.lat_loc[self.ind_next], 'rs', markersize=10)
-            plt.plot(self.data_path_lon, self.data_path_lat, 'b-', linewidth = 5)
-            plt.colorbar()
-            plt.xlabel("Lon [deg]")
-            plt.ylabel("Lat [deg]")
-            plt.title("Prediction error")
-
-            plt.subplot(223)
-            plt.scatter(self.lon_loc, self.lat_loc, c=self.mu_real, vmin=15, vmax=36, s = 500, alpha = 1, cmap="Paired")
-            plt.plot(self.lon_loc[self.ind_pre], self.lat_loc[self.ind_pre], 'kx', markersize=10)
-            plt.plot(self.lon_loc[self.ind_cand], self.lat_loc[self.ind_cand], 'go', markersize=10)
-            plt.plot(self.lon_loc[self.ind_next], self.lat_loc[self.ind_next], 'rs', markersize=10)
-            plt.plot(self.data_path_lon, self.data_path_lat, 'b-', linewidth = 5)
-            plt.colorbar()
-            plt.xlabel("Lon [deg]")
-            plt.ylabel("Lat [deg]")
-            plt.title("True field")
-
-            plt.subplot(224)
-            plt.scatter(lon_cand_plot, lat_cand_plot, c=eibv, s=500, cmap="Paired")
-            plt.plot(self.lon_loc[self.ind_pre], self.lat_loc[self.ind_pre], 'kx', markersize=10)
-            plt.plot(self.lon_loc[self.ind_now], self.lat_loc[self.ind_now], 'bx', markersize=30)
-            plt.plot(self.lon_loc[self.ind_cand], self.lat_loc[self.ind_cand], 'go', markersize=10)
-            plt.plot(self.lon_loc[self.ind_next], self.lat_loc[self.ind_next], 'r*', markersize=30)
-            plt.plot(self.data_path_lon, self.data_path_lat, 'b-', linewidth = 5)
-            plt.colorbar()
-            plt.xlabel("Lon [deg]")
-            plt.ylabel("Lat [deg]")
-            plt.title("EIBV path planning")
-
-            plt.savefig(self.figpath + "P_{:04d}.png".format(self.counter_plot_simulation))
-            self.counter_plot_simulation = self.counter_plot_simulation + 1
-            plt.close("all")
-            pass
         self.data_path_lat.append(self.lat_loc[self.ind_next])
         self.data_path_lon.append(self.lon_loc[self.ind_next])
+        self.data_path_depth.append(self.depth_loc[self.ind_next])
         print("Finding next waypoint takes: ", t2 - t1)
         self.updateF(self.ind_next)
 
