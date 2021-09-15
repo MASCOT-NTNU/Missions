1ebb47d99193e19619dbcce59014b03dae2d5d8b
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-09-16 01:34:57 +0200

    Working_everything

--- Adaptive_script/Porto/func_test.py
+++ Adaptive_script/Porto/func_test.py
@@ -82,14 +82,81 @@ for i in range(len(s)):
 
 
 #%%
+import matplotlib.pyplot as plt
 import h5py
-data_path = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Sep_Prior/Merged/Merged_North_Heavy_2016_SEP_1.h5"
+# data_path = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Sep_Prior/Merged/Merged_North_Heavy_2016_SEP_1.h5"
+data_path = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Sep_Prior/Prior_polygon.h5"
 data = h5py.File(data_path, 'r')
-lat = data.get("lat")
-lon = data.get("lon")
-depth = data.get("depth")
-salinity = data.get("salinity")
-plt.scatter(lon[:, :, 0], lat[:, :, 0], c = salinity[:, :, 0], cmap = "Paired")
+lat = data.get("lat_selected")
+lon = data.get("lon_selected")
+depth = data.get("depth_selected")
+salinity = data.get("salinity_selected")
+plt.scatter(lon[:, 0], lat[:, 0], c = salinity[:, 0], cmap = "Paired")
 plt.colorbar()
 plt.show()
+
+#%%
+
+grid_path = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Sep_Prior/grid.txt"
+grid = np.loadtxt(grid_path, delimiter=", ")
+
+lat = grid[:, 0]
+lon = grid[:, 1]
+# depth = grid[:, 2]
+dist_tolerance =
+plt.plot(lon, lat, 'k.')
+plt.show()
+
+
+def find_neighbour_points(loc):
+    x, y = loc
+    distX = X - x
+    distY = Y - y
+    dist = np.sqrt(distX ** 2 + distY ** 2)
+    ind = np.where(dist <= dist_tolerance)[0]
+    return ind
+
+def find_next(ind):
+    id = np.random.randint(len(ind))
+    return ind[id]
+
+def find_nearest_path(a, b):
+    xa, ya = a
+    xb, yb = b
+    xnext = xa
+    ynext = ya
+    path_x = []
+    path_y = []
+    path_x.append(xnext)
+    path_y.append(ynext)
+    counter = 0
+    figpath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Porto/Setup/Pre_survey/fig/"
+    while True:
+        if xnext == xb and ynext == yb:
+            break
+        else:
+            ind_next = find_next(find_neighbour_points([xnext, ynext]))
+            print(ind_next)
+            xnext = X[ind_next]
+            ynext = Y[ind_next]
+            plt.figure()
+            plt.plot(X, Y, 'k.')
+            plt.plot(xnext, ynext, 'r.')
+            plt.plot(path_x, path_y, 'b-')
+            plt.savefig(figpath + "P_{:04d}.png".format(counter))
+            plt.close("all")
+            path_x.append(xnext)
+            path_y.append(ynext)
+            counter = counter + 1
+            print(counter)
+        if counter > 100:
+            break
+
+
+find_nearest_path([0, 0], [4, 4])
+
+#%%
+path = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Sep_Prior/Prior_polygon.h5'
+
+
 
