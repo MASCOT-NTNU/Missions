1e2e51420a452dc793c80229b466dea73f62dc60
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-10-19 11:08:44 +0200

    Work_on_angle_generator

--- Adaptive_script/Porto/func_test.py
+++ Adaptive_script/Porto/func_test.py
@@ -1,3 +1,18 @@
+datapath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Sep_Prior/Merged_all/South_Heavy_all.h5"
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+
+data = h5py.File(datapath, 'r')
+lat = np.array(data.get("lat"))
+lon = np.array(data.get("lon"))
+depth = np.array(data.get("depth"))
+salinity = np.array(data.get("salinity"))
+
+
+
+
+#%%
 import PySimpleGUI as sg
 import decorator
 
