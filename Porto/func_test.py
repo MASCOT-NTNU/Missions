4c0ad353e8fcbc0ff22aaff12f8155c48fd56a36
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-10-21 14:01:25 +0200

    Merging_data

--- Porto/func_test.py
+++ Porto/func_test.py
@@ -1,3 +1,36 @@
+import h5py
+import numpy as np
+import time
+
+
+datapath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Nov_Prior/Nov2019_sal_1.h5"
+t1 = time.time()
+data = h5py.File(datapath, 'r')
+lat = np.array(data.get("lat"))
+lon = np.array(data.get("lon"))
+depth = np.array(data.get("depth"))
+salinity = np.array(data.get("salinity"))
+t2 = time.time()
+print("Time consuemed: ", t2 - t1)
+
+#%%
+print(lat.shape)
+print(salinity.shape)
+print(depth.shape)
+print(lon.shape)
+#%%
+import matplotlib.pyplot as plt
+
+plt.scatter(lon[:, :, 0], lat[:, :, 0], c = salinity[100, :, :, 0], cmap = "Paired")
+plt.colorbar()
+plt.show()
+#%%
+
+[print(np.nanmean(depth[:, :, :, i])) for i in range(depth.shape[3])]
+
+
+
+#%%
 import multiprocessing
 from Porto.usr_func import test
 
