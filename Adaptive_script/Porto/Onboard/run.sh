260b91ca0ddcf77ad3332c455ee62e672d074b7b
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-10-24 17:26:34 +0200

    System_done

--- Adaptive_script/Porto/Onboard/run.sh
+++ Adaptive_script/Porto/Onboard/run.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
-# python3 Initialisation/EnvInitialisation.py
-# python3 Pre_survey/Pre_surveyor.py
+python3 Initialisation/EnvInitialisation.py
+python3 Pre_survey/Pre_surveyor.py
 # python3 Pre_survey/Pre_surveyor.py > /dev/null 2>&1 &
-# echo "Pre survey is finished"
+echo "Pre survey is finished"
 
 python3 Pre_survey/PostProcessor.py
 echo "Post processing is finished!"
@@ -17,5 +17,5 @@ echo "Grid is generated successfully!"
 python3 Adaptive/PreProcessor.py
 echo "Preprocessing is done! Ready to go with adaptive mission"
 
-python3 Adaptive/MASCOT.py
-echo "Mission Complete!"
+# python3 Adaptive/MASCOT.py
+# echo "Mission Complete!"
