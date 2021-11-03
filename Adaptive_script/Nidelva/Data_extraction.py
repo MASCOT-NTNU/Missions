e1f573a4d7e767b69d85c69acac3fd244df9a9a4
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-11-03 15:54:33 +0100

    Cleaning_finished_today

--- Adaptive_script/Nidelva/Data_extraction.py
+++ Adaptive_script/Nidelva/Data_extraction.py
@@ -1,84 +1,120 @@
-datapath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/May27/Data/'
-import matplotlib.pyplot as plt
-# datapath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/July06/Adaptive/Data/'
-# figpath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/July06/Adaptive/fig/'
+#!/usr/bin/env python3
+__author__ = "Yaolin Ge"
+__copyright__ = "Copyright 2021, The MASCOT Project, NTNU (https://wiki.math.ntnu.no/mascot)"
+__credits__ = ["Yaolin Ge"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Yaolin Ge"
+__email__ = "yaolin.ge@ntnu.no"
+__status__ = "UnderDevelopment"
+
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams.update({'font.size': 12})
+plt.rcParams.update({'font.style': 'oblique'})
 from datetime import datetime
-import os
 from Adaptive_script.Nidelva.usr_func import *
 
 
-#% Data extraction from the raw data
-rawTemp = pd.read_csv(datapath + "Temperature.csv", delimiter=', ', header=0, engine='python')
-rawLoc = pd.read_csv(datapath + "EstimatedState.csv", delimiter=', ', header=0, engine='python')
-rawSal = pd.read_csv(datapath + "Salinity.csv", delimiter=', ', header=0, engine='python')
-rawDepth = pd.read_csv(datapath + "Depth.csv", delimiter=', ', header=0, engine='python')
-# rawGPS = pd.read_csv(datapath + "GpsFix.csv", delimiter=', ', header=0, engine='python')
-# rawCurrent = pd.read_csv(datapath + "EstimatedStreamVelocity.csv", delimiter=', ', header=0, engine='python')
+class EXCEL2TXT:
 
-# To group all the time stamp together, since only second accuracy matters
-rawSal.iloc[:, 0] = np.ceil(rawSal.iloc[:, 0])
-rawTemp.iloc[:, 0] = np.ceil(rawTemp.iloc[:, 0])
-rawCTDTemp = rawTemp[rawTemp.iloc[:, 2] == 'SmartX']
-rawLoc.iloc[:, 0] = np.ceil(rawLoc.iloc[:, 0])
-rawDepth.iloc[:, 0] = np.ceil(rawDepth.iloc[:, 0])
-rawDepth.iloc[:, 0] = np.ceil(rawDepth.iloc[:, 0])
+    datapath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/May27/Data/'
+    # datapath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/July06/Adaptive/Data/'
+    # figpath = '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Nidelva/July06/Adaptive/fig/'
 
-depth_ctd = rawDepth[rawDepth.iloc[:, 2] == 'SmartX']["value (m)"].groupby(rawDepth["timestamp"]).mean()
-depth_dvl = rawDepth[rawDepth.iloc[:, 2] == 'DVL']["value (m)"].groupby(rawDepth["timestamp"]).mean()
-depth_est = rawLoc["depth (m)"].groupby(rawLoc["timestamp"]).mean()
+    def __init__(self):
+        print("hello world")
+        pass
 
-# indices used to extract data
-lat_origin = rawLoc["lat (rad)"].groupby(rawLoc["timestamp"]).mean()
-lon_origin = rawLoc["lon (rad)"].groupby(rawLoc["timestamp"]).mean()
-x_loc = rawLoc["x (m)"].groupby(rawLoc["timestamp"]).mean()
-y_loc = rawLoc["y (m)"].groupby(rawLoc["timestamp"]).mean()
-z_loc = rawLoc["z (m)"].groupby(rawLoc["timestamp"]).mean()
-depth = rawLoc["depth (m)"].groupby(rawLoc["timestamp"]).mean()
-time_loc = rawLoc["timestamp"].groupby(rawLoc["timestamp"]).mean()
-time_sal= rawSal["timestamp"].groupby(rawSal["timestamp"]).mean()
-time_temp = rawCTDTemp["timestamp"].groupby(rawCTDTemp["timestamp"]).mean()
-dataSal = rawSal["value (psu)"].groupby(rawSal["timestamp"]).mean()
-dataTemp = rawCTDTemp.iloc[:, -1].groupby(rawCTDTemp["timestamp"]).mean()
+    def load_raw_data(self):
+        # % Data extraction from the raw data
+        self.rawTemp = pd.read_csv(self.datapath + "Temperature.csv", delimiter=', ', header=0, engine='python')
+        self.rawLoc = pd.read_csv(self.datapath + "EstimatedState.csv", delimiter=', ', header=0, engine='python')
+        self.rawSal = pd.read_csv(self.datapath + "Salinity.csv", delimiter=', ', header=0, engine='python')
+        self.rawDepth = pd.read_csv(self.datapath + "Depth.csv", delimiter=', ', header=0, engine='python')
+        # rawGPS = pd.read_csv(datapath + "GpsFix.csv", delimiter=', ', header=0, engine='python')
+        # rawCurrent = pd.read_csv(datapath + "EstimatedStreamVelocity.csv", delimiter=', ', header=0, engine='python')
+        print("Raw data is loaded successfully!")
 
-#% Rearrange data according to their timestamp
-data = []
-time_mission = []
-x = []
-y = []
-z = []
-d = []
-sal = []
-temp = []
-lat = []
-lon = []
+    def group_raw_data_based_on_timestamp(self):
 
-for i in range(len(time_loc)):
-    if np.any(time_sal.isin([time_loc.iloc[i]])) and np.any(time_temp.isin([time_loc.iloc[i]])):
-        time_mission.append(time_loc.iloc[i])
-        x.append(x_loc.iloc[i])
-        y.append(y_loc.iloc[i])
-        z.append(z_loc.iloc[i])
-        d.append(depth.iloc[i])
-        lat_temp = rad2deg(lat_origin.iloc[i]) + rad2deg(x_loc.iloc[i] * np.pi * 2.0 / circumference)
-        lat.append(lat_temp)
-        lon.append(rad2deg(lon_origin.iloc[i]) + rad2deg(y_loc.iloc[i] * np.pi * 2.0 / (circumference * np.cos(deg2rad(lat_temp)))))
-        sal.append(dataSal[time_sal.isin([time_loc.iloc[i]])].iloc[0])
-        temp.append(dataTemp[time_temp.isin([time_loc.iloc[i]])].iloc[0])
-    else:
-        print(datetime.fromtimestamp(time_loc.iloc[i]))
-        continue
+        # To group all the time stamp together, since only second accuracy matters
+        self.rawSal.iloc[:, 0] = np.ceil(self.rawSal.iloc[:, 0])
+        self.rawTemp.iloc[:, 0] = np.ceil(self.rawTemp.iloc[:, 0])
+        self.rawCTDTemp = self.rawTemp[self.rawTemp.iloc[:, 2] == 'SmartX'] # 'SmartX' because CTD sensor is SmartX
+        self.rawLoc.iloc[:, 0] = np.ceil(self.rawLoc.iloc[:, 0])
+        self.rawDepth.iloc[:, 0] = np.ceil(self.rawDepth.iloc[:, 0])
+        self.rawDepth.iloc[:, 0] = np.ceil(self.rawDepth.iloc[:, 0])
+        print("Raw data is grouped successfully!")
 
-lat = np.array(lat).reshape(-1, 1)
-lon = np.array(lon).reshape(-1, 1)
-x = np.array(x).reshape(-1, 1)
-y = np.array(y).reshape(-1, 1)
-z = np.array(z).reshape(-1, 1)
-d = np.array(d).reshape(-1, 1)
-sal = np.array(sal).reshape(-1, 1)
-temp = np.array(temp).reshape(-1, 1)
-time_mission = np.array(time_mission).reshape(-1, 1)
+    def extract_all_data(self):
+        self.depth_ctd = self.rawDepth[self.rawDepth.iloc[:, 2] == 'SmartX']["value (m)"].groupby(self.rawDepth["timestamp"]).mean()
+        self.depth_dvl = self.rawDepth[self.rawDepth.iloc[:, 2] == 'DVL']["value (m)"].groupby(self.rawDepth["timestamp"]).mean()
+        self.depth_est = self.rawLoc["depth (m)"].groupby(self.rawLoc["timestamp"]).mean()
 
-datasheet = np.hstack((time_mission, lat, lon, x, y, z, d, sal, temp))
-# np.savetxt(figpath + "../data.txt", datasheet, delimiter = ",")
+        # indices used to extract data
+        self.lat_origin = self.rawLoc["lat (rad)"].groupby(self.rawLoc["timestamp"]).mean()
+        self.lon_origin = self.rawLoc["lon (rad)"].groupby(self.rawLoc["timestamp"]).mean()
+        self.x_loc = self.rawLoc["x (m)"].groupby(self.rawLoc["timestamp"]).mean()
+        self.y_loc = self.rawLoc["y (m)"].groupby(self.rawLoc["timestamp"]).mean()
+        self.z_loc = self.rawLoc["z (m)"].groupby(self.rawLoc["timestamp"]).mean()
+        self.depth = self.rawLoc["depth (m)"].groupby(self.rawLoc["timestamp"]).mean()
+        self.time_loc = self.rawLoc["timestamp"].groupby(self.rawLoc["timestamp"]).mean()
+        self.time_sal= self.rawSal["timestamp"].groupby(self.rawSal["timestamp"]).mean()
+        self.time_temp = self.rawCTDTemp["timestamp"].groupby(self.rawCTDTemp["timestamp"]).mean()
+        self.dataSal = self.rawSal["value (psu)"].groupby(self.rawSal["timestamp"]).mean()
+        self.dataTemp = self.rawCTDTemp.iloc[:, -1].groupby(self.rawCTDTemp["timestamp"]).mean()
+        print("Data is extracted successfully!")
+
+    def save_data(self):
+        #% Rearrange data according to their timestamp
+        self.load_raw_data()
+        self.group_raw_data_based_on_timestamp()
+        self.extract_all_data()
+        data = []
+        time_mission = []
+        x = []
+        y = []
+        z = []
+        d = []
+        sal = []
+        temp = []
+        lat = []
+        lon = []
+
+        for i in range(len(self.time_loc)):
+            if np.any(self.time_sal.isin([self.time_loc.iloc[i]])) and np.any(self.time_temp.isin([self.time_loc.iloc[i]])):
+                time_mission.append(self.time_loc.iloc[i])
+                x.append(self.x_loc.iloc[i])
+                y.append(self.y_loc.iloc[i])
+                z.append(self.z_loc.iloc[i])
+                d.append(self.depth.iloc[i])
+                lat_temp = rad2deg(self.lat_origin.iloc[i]) + rad2deg(self.x_loc.iloc[i] * np.pi * 2.0 / circumference)
+                lat.append(lat_temp)
+                lon.append(rad2deg(self.lon_origin.iloc[i]) + rad2deg(self.y_loc.iloc[i] * np.pi * 2.0 / (circumference * np.cos(deg2rad(lat_temp)))))
+                sal.append(self.dataSal[self.time_sal.isin([self.time_loc.iloc[i]])].iloc[0])
+                temp.append(self.dataTemp[self.time_temp.isin([self.time_loc.iloc[i]])].iloc[0])
+            else:
+                print(datetime.fromtimestamp(self.time_loc.iloc[i]))
+                continue
+
+        self.lat = np.array(lat).reshape(-1, 1)
+        self.lon = np.array(lon).reshape(-1, 1)
+        self.x = np.array(x).reshape(-1, 1)
+        self.y = np.array(y).reshape(-1, 1)
+        self.z = np.array(z).reshape(-1, 1)
+        self.d = np.array(d).reshape(-1, 1)
+        self.sal = np.array(sal).reshape(-1, 1)
+        self.temp = np.array(temp).reshape(-1, 1)
+        self.time_mission = np.array(time_mission).reshape(-1, 1)
+
+        self.datasheet = np.hstack((time_mission, lat, lon, x, y, z, d, sal, temp))
+        # np.savetxt(figpath + "../data.txt", datasheet, delimiter = ",")
+        print("Data is saved successfully!")
+
+if __name__ == "__main__":
+    a = EXCEL2TXT()
+    a.save_data()
+
