4b5044fca02a6496748a3ffb216b6b0dde2f377e
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-11-09 14:37:52 +0100

    Simulation

--- Porto/PlumeForcast/Clairvoyant.py
+++ Porto/PlumeForcast/Clairvoyant.py
@@ -12,65 +12,75 @@ import numpy as np
 import time
 import h5py
 import os
+import urllib.request
 import matplotlib.pyplot as plt
+from dateutil import rrule
+from datetime import datetime
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams.update({'font.size': 12})
 plt.rcParams.update({'font.style': 'oblique'})
 
+server = True
+if not server:
+    path_data_new = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Maretec/Data/"
+    figpath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Porto/PlumeForcast/fig/"
+else:
+    path_data_new = "/home/ahomea/y/yaoling/MASCOT/Porto_Data_Processing/MOHID/Data/"
+    figpath = "/home/ahomea/y/yaoling/MASCOT/Porto_Data_Processing/MOHID/fig/"
 
-class Clairvoyant:
-    path_maretec = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Maretec/Douro/"
+
+class DataFetcher:
+    string_start = '20210915'
+    string_end = '20211120' # this needs to be two days more than the current day since it needs to populate more and cut off one day
 
     def __init__(self):
         pass
 
-    def load_maretec_data(self, file):
-        print("Now it will load the Maretec data...")
-        t1 = time.time()
-        self.data = h5py.File(self.path_maretec + file + "/WaterProperties.hdf5", 'r')
-        self.grid = self.data.get('Grid')
-        self.lat = np.array(self.grid.get("Latitude"))[:-1, :-1]
-        self.lon = np.array(self.grid.get("Longitude"))[:-1, :-1]
-        self.depth = []
-        self.salinity = []
-        for i in range(1, 26):
-            string_z = "Vertical_{:05d}".format(i)
-            string_sal = "salinity_{:05d}".format(i)
-            self.depth.append(np.mean(np.array(self.grid.get("VerticalZ").get(string_z)), axis = 0))
-            self.salinity.append(np.mean(np.array(self.data.get("Results").get("salinity").get(string_sal)), axis = 0))
-        self.depth = np.array(self.depth)
-        self.salinity = np.array(self.salinity)
-        t2 = time.time()
-        print("Maretec data is loaded correctly, time consumed: ", t2 - t1)
+    def get_all_date(self):
+        '''
+        get all the available date
+        '''
+        from dateutil import rrule
+        from datetime import datetime
+        self.string_dates = []
+        for dt in rrule.rrule(rrule.DAILY,dtstart=datetime.strptime(self.string_start, '%Y%m%d'),
+                              until=datetime.strptime(self.string_end, '%Y%m%d')):
+            self.string_dates.append(dt.strftime('%Y%m%d'))
 
-    def visualiseForcast(self):
-        files = os.listdir(self.path_maretec)
-        for file in files:
-            if file.startswith("2021"):
-                self.load_maretec_data(file)
-                fig = plt.figure(figsize = (10, 10))
-                ind_row = np.where(self.lon <= -8.6)[0]
-                ind_col = np.where(self.lon <= -8.6)[1]
-                print(ind_row)
-                print(ind_col)
-                break
+    def fetch_data(self):
+        self.get_all_date()
+        print(self.string_dates)
+        for i in range(len(self.string_dates)):
+            if i < len(self.string_dates) - 1:
+                string_date1 = self.string_dates[i]
+                year1 = string_date1[:4]
+                month1 = string_date1[4:6]
+                day1 = string_date1[6:]
+                string_date2 = self.string_dates[i+1]
+                year2 = string_date2[:4]
+                month2 = string_date2[4:6]
+                day2 = string_date2[6:]
+                print('ftp://Renato:REP_Modelling@ftp.mohid.com/CoLAB_Atlantic/LSTS/Douro/'+year1+'-'+month1+'-'+day1+'_'+year2+'-'+month2+'-'+day2+'/WaterProperties.hdf5',
+                                           path_data_new + 'WaterProperties_'+string_date1+'.hdf5')
+                try:
+                    urllib.request.urlretrieve('ftp://Renato:REP_Modelling@ftp.mohid.com/CoLAB_Atlantic/LSTS/Douro/'+year1+'-'+month1+'-'+day1+'_'+year2+'-'+month2+'-'+day2+'/WaterProperties.hdf5',
+                                               path_data_new + 'WaterProperties_'+string_date1+'.hdf5')
+                except:
+                    print("Something wrong, but I ignored")
+                    pass
+                print(i)
+        print("data is fetched successfully!")
 
-class MaretecDataHandler:
-    data_path = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Maretec/Douro/"
-    figpath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Missions/Porto/PlumeForcast/fig/"
+
+class Clairvoyant:
+
     def __init__(self):
-
-        # self.loaddata()
-        # self.loadDelft3DSurface()
-        # self.loadSatellite()
-        # self.getsimilarDelft()
-        # self.plotSurfaceData()
         pass
 
     def loaddata(self, file):
         print("Now it will load the Maretec data...")
         t1 = time.time()
-        self.data = h5py.File(self.data_path + file +"/WaterProperties.hdf5", 'r')
+        self.data = h5py.File(path_data_new + file, 'r')
         self.grid = self.data.get('Grid')
         self.lat = np.array(self.grid.get("Latitude"))[:-1, :-1]
         self.lon = np.array(self.grid.get("Longitude"))[:-1, :-1]
@@ -88,9 +98,9 @@ class MaretecDataHandler:
 
     def checkFolder(self):
         i = 0
-        while os.path.exists(self.figpath + "P%s" % i):
+        while os.path.exists(figpath + "P%s" % i):
             i += 1
-        self.figpath = self.figpath + "P%s" % i
+        self.figpath = figpath + "P%s" % i
         if not os.path.exists(self.figpath):
             print(self.figpath + " is created")
             os.mkdir(self.figpath)
@@ -100,7 +110,7 @@ class MaretecDataHandler:
     def visualiseData(self):
         self.checkFolder()
         print("Here it comes the plotting for the updated results from Maretec.")
-        files = os.listdir(self.data_path)
+        files = os.listdir(path_data_new)
         files.sort()
         counter = 0
         for i in range(len(files)):
@@ -122,14 +132,11 @@ class MaretecDataHandler:
                     counter = counter + 1
                 # plt.show()
 
-    @staticmethod
-    def deg2rad(deg):
-        return deg / 180 * np.pi
-
 if __name__ == "__main__":
-    a = MaretecDataHandler()
-    a.visualiseData()
-    # a = Clairvoyant()
-    # a.visualiseForcast()
+    a = DataFetcher()
+    a.fetch_data()
+
+    b = Clairvoyant()
+    b.visualiseData()
 
 
--- Porto/func_test.py
+++ Porto/func_test.py
@@ -1,11 +1,112 @@
+"""Minesweeper
+
+Exercises
+
+1. What does the `seed(0)` function call do?
+2. Change the number of bombs on the grid.
+3. Change the size of the grid.
+
+"""
+
+from random import randrange, seed
+from turtle import *
+
+from freegames import floor, square
+
+seed(0)
+bombs = {}
+shown = {}
+counts = {}
+
+
+def initialize():
+    "Initialize `bombs`, `counts`, and `shown` grids."
+    for x in range(-250, 250, 50):
+        for y in range(-250, 250, 50):
+            bombs[x, y] = False
+            shown[x, y] = False
+            counts[x, y] = -1
+
+    for count in range(8):
+        x = randrange(-200, 200, 50)
+        y = randrange(-200, 200, 50)
+        bombs[x, y] = True
+
+    for x in range(-200, 200, 50):
+        for y in range(-200, 200, 50):
+            total = 0
+            for i in (-50, 0, 50):
+                for j in (-50, 0, 50):
+                    total += bombs[x + i, y + j]
+            counts[x, y] = total
+
+
+def stamp(x, y, text):
+    "Display `text` at coordinates `x` and `y`."
+    square(x, y, 50, 'white')
+    color('black')
+    write(text, font=('Arial', 50, 'normal'))
+
+
+def draw():
+    "Draw the initial board grid."
+    for x in range(-200, 200, 50):
+        for y in range(-200, 200, 50):
+            stamp(x, y, '?')
+
+
+def end():
+    "Draw the bombs as X's on the grid."
+    for x in range(-200, 200, 50):
+        for y in range(-200, 200, 50):
+            if bombs[x, y]:
+                stamp(x, y, 'X')
+
+
+def tap(x, y):
+    "Respond to screen click at `x` and `y` coordinates."
+    x = floor(x, 50)
+    y = floor(y, 50)
+
+    if bombs[x, y]:
+        end()
+        return
+
+    pairs = [(x, y)]
+
+    while pairs:
+        x, y = pairs.pop()
+        stamp(x, y, counts[x, y])
+        shown[x, y] = True
+
+        if counts[x, y] == 0:
+            for i in (-50, 0, 50):
+                for j in (-50, 0, 50):
+                    pair = x + i, y + j
+                    if not shown[pair]:
+                        pairs.append(pair)
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+initialize()
+draw()
+onscreenclick(tap)
+done()
+
+#%%
 import h5py
 import numpy as np
 import time
 
 
-datapath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Nov_Prior/Merged/Merged_North_Mild_2019_sal_2.h5"
+# datapath = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Nov_Prior/Merged/Merged_North_Mild_2019_sal_2.h5"
+datapath = "Hydrodynamic.hdf5"
 t1 = time.time()
 data = h5py.File(datapath, 'r')
+
+#%%
 lat = np.array(data.get("lat"))
 lon = np.array(data.get("lon"))
 depth = np.array(data.get("depth"))
@@ -15,6 +116,28 @@ print("Time consuemed: ", t2 - t1)
 
 
 #%%
+data_path = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Porto/Prior/Maretec/Data/Hydrodynamic_20210915.hdf5"
+import h5py
+
+data = h5py.File(data_path, 'r')
+data.get("Grid")
+
+#%%
+import urllib.request
+
+# urllib.urlretrieve('ftp://server/path/to/file', 'file')
+# if you need to pass credentials:
+urllib.request.urlretrieve('ftp://Renato:REP_Modelling@ftp.mohid.com/CoLAB_Atlantic/LSTS/Douro/2021-11-06_2021-11-07/Hydrodynamic.hdf5', 'Hydrodynamic.hdf5')
+
+#%%
+
+try:
+    print("good")
+    # urllib.request.urlretrieve('ftp://Renato:REP_Modelling@ftp.mohid.com/CoLAB_Atlantic/LSTS/Douro/2021-11-06_2021-11-07/Hydrodynamic.hdf5','Hydrodynamic.hdf5')
+except:
+    pass
+
+print("Hello world")
 
 #%%
 print(lat.shape)
@@ -29,9 +152,37 @@ plt.colorbar()
 plt.show()
 #%%
 
-[print(np.nanmean(depth[:, :, :, i])) for i in range(depth.shape[3])]
+# [print(np.nanmean(depth[:, :, :, i])) for i in range(depth.shape[3])]
 
+from numba import jit, cuda
+import numpy as np
+# to measure exec time
+from timeit import default_timer as timer
 
+# normal function to run on cpu
+def func(a):
+    for i in range(10000000):
+        a[i] += 1
+
+# function optimized to run on gpu
+
+@jit(target="cuda")
+def func2(b):
+    for i in range(10000000):
+        b[i] += 1
+
+if __name__ == "__main__":
+    n = 10000000
+    a = np.ones(n, dtype=np.float64)
+    b = np.ones(n, dtype=np.float32)
+
+    start = timer()
+    func(a)
+    print("without GPU:", timer() - start)
+
+    start = timer()
+    func2(a)
+    print("with GPU:", timer() - start)
 
 #%%
 import multiprocessing
